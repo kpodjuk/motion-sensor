e7c15d46be2cdf19c3c753135122f2b9d977dfa9
kpodjuk <62662345+kpodjuk@users.noreply.github.com>
2024-03-15 04:51:08 +0100

    websocket

---
+++ include/main.h
@@ -0,0 +1,77 @@
+#include <string.h>
+#include "arduino-timer.h"
+
+//  Pin definitions
+const int powerButtonPin = D4; // green wire
+const int powerLightPin = D2;  // blue  wire
+const long interval = 30000;   // interval when rapoerting wifi status
+const long delayWhenMakingSure = 40000;
+
+#define JSON_MAXLENGTH 200
+
+auto timer = timer_create_default();
+
+// ### COLORS - Defines that help with colorizing serial output ###
+#define Black "\e[0;30m"
+#define Red "\e[0;31m"
+#define Green "\e[0;32m"
+#define Yellow "\e[0;33m"
+#define Blue "\e[0;34m"
+#define Purple "\e[0;35m"
+#define Cyan "\e[0;36m"
+#define White "\e[0;37m"
+#define EndColor "\e[0"
+// ### COLORS ###
+
+typedef enum
+{
+    CURRENT, // do nothing
+    OFF,     // make sure it's off
+    ON,      // make sure it's on
+} desiredPcStatus_t;
+
+desiredPcStatus_t desiredPcStatus;
+
+ESP8266WiFiMulti wifiMulti;                 // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
+StaticJsonDocument<JSON_MAXLENGTH> jsonDoc; // JSON document received with websocket
+ESP8266WebServer server(80);                // Create a webserver object that listens for HTTP request on port 80
+WebSocketsServer webSocket(81);             // create a websocket server on port 81
+File fsUploadFile;                          // a File variable to temporarily store the received file
+const char *ssid = "PC_Wemos";              // The name of the Wi-Fi network that will be created
+const char *password = "";                  // The password required to connect to it, leave blank for an open network
+const char *OTAName = "ESP8266";            // A name and a password for the OTA service
+const char *OTAPassword = "esp8266";
+const char *mdnsName = "esp8266"; // Domain name for the mDNS responder
+unsigned long previousMillis;
+bool previousPcStatus;
+
+// ************ Function definitions ************
+bool makeSure(void *);
+
+void startWiFi();      // Start a Wi-Fi access point, and try to connect to some given access points. Then wait for either an AP or STA connection
+void startOTA();       // Start the OTA service
+void startSPIFFS();    // Start the SPIFFS and list all contents
+void startWebSocket(); // Start a WebSocket server
+void startMDNS();      // Start the mDNS responder
+void startServer();    // Start a HTTP server with a file read handler and an upload handler
+String formatBytes(size_t bytes);
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t lenght);
+bool handleFileRead(String path);
+void sendStatus();
+void handleNotFound();
+String getContentType(String filename);
+void pressPowerButton();
+void turnOff();
+void turnOn();
+void checkPowerLightPin();
+void refreshStatusIfNeeded();
+void raportStatusOnSerial();
+// WL_NO_SHIELD        = 255,   // for compatibility with WiFi Shield library
+// WL_IDLE_STATUS      = 0,
+// WL_NO_SSID_AVAIL    = 1,
+// WL_SCAN_COMPLETED   = 2,
+// WL_CONNECTED        = 3,
+// WL_CONNECT_FAILED   = 4,
+// WL_CONNECTION_LOST  = 5,
+// WL_WRONG_PASSWORD   = 6,
+// WL_DISCONNECTED     = 7
--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,373 @@
-#include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ArduinoOTA.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <FS.h>
+#include <WebSocketsServer.h>
+#include "ArduinoJson.h"
+#include "main.h"
+#include <arduino-timer.h>
 
 void setup()
 {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
   pinMode(0, INPUT);
 
-  Serial.begin(115200);             // Ustawienie prędkości transmisji
-  Serial.println("Witaj swiecie!"); // Jednorazowe wysłanie tekstu
-}
+  Serial.begin(115200); // Start the Serial communication to send messages to the computer
+  delay(10);
+  Serial.println("\r\n");
 
+  startWiFi(); // Start a Wi-Fi access point, and try to connect to some given access points. Then wait for either an AP or STA connection
+
+  startOTA();    // Start the OTA service
+  startSPIFFS(); // Start the SPIFFS and list all contents
+
+  startWebSocket(); // Start a WebSocket server
+  startServer();    // Start a HTTP server with a file read handler and an upload handler
+}
+#define RAPORT_WIFI_STATUS
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  Serial.println("Witaj swiecie!"); // Jednorazowe wysłanie tekstu
-  bool state = digitalRead(0);
-  Serial.println(state);
-  delay(1000);
+
+  webSocket.loop(); // constantly check for websocket events
+#ifdef RAPORT_WIFI_STATUS
+  raportStatusOnSerial();
+#endif
+  server.handleClient(); // run the server
+  ArduinoOTA.handle();   // listen for OTA events
 }
 
-// put function definitions here:
-int myFunction(int x, int y)
+void startWiFi()
+{ // Start a Wi-Fi access point, and try to connect to some given access points. Then wait for either an AP or STA connection
+  // WiFi.softAP(ssid, password); // Start the access point
+  // Serial.print("Access Point \"");
+  // Serial.print(ssid);
+  // Serial.println("\" started\r\n");
+
+  // will it connect to multiple networks or only one?
+  WiFi.begin("Orange_Swiatlowod_Gora", "mlekogrzybowe"); // add Wi-Fi networks you want to connect to
+  // wifiMulti.addAP("Orange_Swiatlowod_E8A0", "mlekogrzybowe);
+
+  Serial.println("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  { // Wait for the Wi-Fi to connect
+    delay(250);
+    Serial.print('.');
+  }
+  Serial.println("\r\n");
+
+  Serial.println("\r\n");
+
+  WiFi.setAutoReconnect(true);
+  WiFi.persistent(true);
+}
+
+void startOTA()
+{ // Start the OTA service
+  ArduinoOTA.setHostname(OTAName);
+  ArduinoOTA.setPassword(OTAPassword);
+
+  ArduinoOTA.onStart([]()
+                     { Serial.println("Start"); });
+  ArduinoOTA.onEnd([]()
+                   { Serial.println("\r\nEnd"); });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
+                        { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); });
+  ArduinoOTA.onError([](ota_error_t error)
+                     {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed"); });
+  ArduinoOTA.begin();
+  Serial.println("OTA ready\r\n");
+}
+
+void startSPIFFS()
+{                 // Start the SPIFFS and list all contents
+  SPIFFS.begin(); // Start the SPI Flash File System (SPIFFS)
+  Serial.println("SPIFFS started. Contents:");
+  {
+    Dir dir = SPIFFS.openDir("/");
+    while (dir.next())
+    { // List the file system contents
+      String fileName = dir.fileName();
+      size_t fileSize = dir.fileSize();
+      Serial.printf("\tFS File: %s, size: %s\r\n", fileName.c_str(), formatBytes(fileSize).c_str());
+    }
+    Serial.printf("\n");
+  }
+}
+
+void startWebSocket()
+{                                    // Start a WebSocket server
+  webSocket.begin();                 // start the websocket server
+  webSocket.onEvent(webSocketEvent); // if there's an incomming websocket message, go to function 'webSocketEvent'
+  Serial.println("WebSocket server started.");
+}
+
+void startServer()
+{                                           // Start a HTTP server with a file read handler and an upload handler
+  server.on("/edit.html", HTTP_POST, []() { // If a POST request is sent to the /edit.html address,
+    server.send(200, "text/plain", "");
+  });
+
+  server.on("/api", HTTP_GET, []()
+            {
+    if (server.argName(0) == "p")
+    {
+      String desiredAction = server.arg(0);
+      String answer;
+      if (desiredAction == "on")
+      {
+        Serial.println("\e[0;35m/api: Turning on!\e[0m");
+        answer = "/api: Turning on if required!";
+        server.send(200, "text/plain", answer);
+      }
+      else if (desiredAction == "off")
+      {
+        Serial.println("\e[0;35m/api: Turning off!\e[0m");
+        server.send(200, "text/plain", "/api: Turning off!");
+      }
+      else if (desiredAction == "toggle")
+      {
+        Serial.println("\e[0;35m/api: Toggling!\e[0m");
+        server.send(200, "text/plain", "/api: Toggling!");
+      }
+      else
+      {
+        Serial.println("\e[0;31m/api: Wrong param value!\e[0m");
+        server.send(200, "text/plain", "/api: Wrong param value!");
+      }
+    } 
+    else
+    {
+      // if no param = send current PC status
+      Serial.println("\e[0;35m/api: Sending PC status!\e[0m");
+      char answerCharArr[25];
+      sprintf(answerCharArr, "/api: PC status: %s", 
+      digitalRead(powerLightPin) == 0 ? "Online" : "Offline");
+      server.send(200, "text/plain", answerCharArr);
+    } });
+
+  server.onNotFound(handleNotFound); // if someone requests any other file or page, go to function 'handleNotFound'
+                                     // and check if the file exists
+
+  server.begin(); // start the HTTP server
+  Serial.println("HTTP server started.");
+}
+
+void handleNotFound()
 {
-  return x + y;
+// if the requested file or page doesn't exist, return a 404 not found error
+#ifdef NO_WEBCLIENT
+  server.send(404, "text/plain", "404: WebClient not available in this build, use /api?p=");
+#else
+  if (!handleFileRead(server.uri()))
+  { // check if the file exists in the flash memory (SPIFFS), if so, send it
+    server.send(404, "text/plain", "404: File Not Found");
+  }
+#endif
+}
+
+bool handleFileRead(String path)
+{ // send the right file to the client (if it exists)
+  Serial.println("handleFileRead: " + path);
+  if (path.endsWith("/"))
+    path += "index.html";                    // If a folder is requested, send the index file
+  String contentType = getContentType(path); // Get the MIME type
+  String pathWithGz = path + ".gz";
+  if (SPIFFS.exists(pathWithGz) || SPIFFS.exists(path))
+  {                                                     // If the file exists, either as a compressed archive, or normal
+    if (SPIFFS.exists(pathWithGz))                      // If there's a compressed version available
+      path += ".gz";                                    // Use the compressed verion
+    File file = SPIFFS.open(path, "r");                 // Open the file
+    size_t sent = server.streamFile(file, contentType); // Send it to the client
+    file.close();                                       // Close the file again
+    Serial.println(String("\tSent file-> ") + path);
+    return true;
+  }
+  Serial.println(String("\tFile Not Found: ") + path); // If the file doesn't exist, return false
+  return false;
+}
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t lenght)
+{ // When a WebSocket message is received
+  Serial.println("tewapol");
+  switch (type)
+  {
+  case WStype_DISCONNECTED: // if the websocket is disconnected
+    Serial.printf("[%u] Disconnected!\n", num);
+    break;
+  case WStype_CONNECTED:
+  { // if a new websocket connection is established
+    IPAddress ip = webSocket.remoteIP(num);
+    Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+  }
+  break;
+  case WStype_TEXT: // if new text data is received
+    Serial.printf("[%u] got WS text: %s\n", num, payload);
+
+    // String payload_str = String((char*) payload);
+
+    // process received JSON
+    DeserializationError error = deserializeJson(jsonDoc, payload);
+
+    // Test if parsing succeeds.
+    if (error)
+    {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      return;
+    }
+
+    // Fetch values.
+    // Most of the time, you can rely on the implicit casts.
+    // In other case, you can do doc["time"].as<long>();
+
+    if (jsonDoc["type"] == "POWER_BUTTON")
+    {
+      Serial.println("Json tells me to: pressPowerButton()");
+      pressPowerButton();
+    }
+    else if (jsonDoc["connected"] == true)
+    {
+      // Serial.println("Json tells me to: sendStatus()");
+      sendStatus();
+    }
+
+    break;
+  }
+}
+
+String formatBytes(size_t bytes)
+{ // convert sizes in bytes to KB and MB
+  if (bytes < 1024)
+  {
+    return String(bytes) + "B";
+  }
+  else if (bytes < (1024 * 1024))
+  {
+    return String(bytes / 1024.0) + "KB";
+  }
+  else if (bytes < (1024 * 1024 * 1024))
+  {
+    return String(bytes / 1024.0 / 1024.0) + "MB";
+  }
+  return String(0);
+}
+
+String getContentType(String filename)
+{ // determine the filetype of a given filename, based on the extension
+  if (filename.endsWith(".html"))
+    return "text/html";
+  else if (filename.endsWith(".css"))
+    return "text/css";
+  else if (filename.endsWith(".js"))
+    return "application/javascript";
+  else if (filename.endsWith(".ico"))
+    return "image/x-icon";
+  else if (filename.endsWith(".gz"))
+    return "application/x-gzip";
+  return "text/plain";
+}
+
+void sendStatus()
+{
+  // send current status to websocket client here (mode, settings for that mode)
+
+  jsonDoc["turnedOn"] = !digitalRead(powerLightPin);
+
+  String statusString;
+  serializeJson(jsonDoc, statusString);
+
+  Serial.print("Sending status:");
+
+  Serial.print(statusString);
+
+  // It's sent to every client connected, not the one who requested it
+  // no harm in that tho
+  webSocket.broadcastTXT(statusString);
+}
+
+void pressPowerButton()
+{
+  const uint16_t delayBetweenPresses = 200;
+
+  // Pin LOW == you pressed power button
+  digitalWrite(powerButtonPin, LOW);
+  Serial.printf("\e[0;33mpowerButtonPin=%i\e[0m\n", LOW);
+  delay(delayBetweenPresses); // too small of a delay and it might not work
+  digitalWrite(powerButtonPin, HIGH);
+  Serial.printf("\e[0;33mpowerButtonPin=%i\e[0m\n", HIGH);
+
+#ifndef NO_WEBCLIENT
+  sendStatus();
+#endif
+}
+
+void raportStatusOnSerial()
+{
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval)
+  {
+    previousMillis = currentMillis;
+
+    Serial.printf("\e[0;34m ############### Info ############### \e[0m \n");
+
+    Serial.printf("Signal strength: %i dBm\n", WiFi.RSSI());
+
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
+
+    Serial.print("localIP: ");
+    Serial.println(WiFi.localIP());
+
+    Serial.print("Status: ");
+
+    char color[20] = "\e[0;31m"; // red by default
+    switch (WiFi.status())
+    {
+    case WL_IDLE_STATUS:
+    {
+      Serial.printf("%sWL_IDLE_STATUS \e[0m\n", color);
+      break;
+    }
+    case WL_NO_SSID_AVAIL:
+    {
+      Serial.printf("%sWL_NO_SSID_AVAIL \e[0m\n", color);
+      break;
+    }
+    case WL_SCAN_COMPLETED:
+    {
+      Serial.printf("%sWL_SCAN_COMPLETED \e[0m\n", color);
+      break;
+    }
+    case WL_CONNECTED:
+    {
+      strcpy(color, "\e[0;32m"); // change to green when connected
+      Serial.printf("%sWL_CONNECTED \e[0m\n", color);
+      break;
+    }
+    case WL_CONNECT_FAILED:
+    {
+      Serial.printf("%sWL_CONNECT_FAILED \e[0m\n", color);
+      break;
+    }
+    case WL_CONNECTION_LOST:
+    {
+      Serial.printf("%sWL_CONNECTION_LOST \e[0m\n", color);
+      break;
+    }
+    case WL_DISCONNECTED:
+    {
+      Serial.printf("%sWL_DISCONNECTED \e[0m\n", color);
+      break;
+    }
+    }
+  }
 }
