5271b2a1b22b7273648ca71e1c8dfe45c1572543
kpodjuk <62662345+kpodjuk@users.noreply.github.com>
2024-03-19 00:09:00 +0100

    holdPinState()

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <ArduinoOTA.h>
 #include <WebSocketsServer.h>
 #include "main.h"
+#include <time.h>
 
 void setup()
 {
@@ -123,6 +124,28 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t lenght)
   }
 }
 
+// Function to set the pin state and hold it for a specified duration in milliseconds
+void holdPinState(int pinState, int duration)
+{
+  // Set the pin state here
+  // For example: digitalWrite(pin, pinState);
+  static uint64_t holdStartTime;
+  // Hold the state for the specified duration
+  if ((millis() - holdStartTime) < duration)
+  {
+    uint64_t holdStartTime = millis();
+    Serial.printf("Hold pin state for %i ms \r\n", (millis() - holdStartTime));
+    digitalWrite(motionSensePin, 0x1); // Set the pin state here
+  }
+  else
+  {
+    // duration passed
+    Serial.println("Hold time passed");
+    digitalWrite(motionSensePin, 0x0); // Set the pin state here
+  }
+}
+
+
 void handleMotionStatus()
 {
   uint64_t currentMillis = millis();
@@ -131,6 +154,13 @@ void handleMotionStatus()
     // Save the last time the sensor was checked
     lastCheckTime = currentMillis;
 
+    // hold status for some time
+    uint8_t pinState = digitalRead(motionSensePin);
+    if (pinState)
+    {
+      // Hold the pin state for 1000ms
+      holdPinState(pinState, 10000);
+    }
 #if 0
     Serial.printf("currentMillis=%i \t D0 status= %i \t D1 status= %i \r\n", 
     (uint32_t)currentMillis, digitalRead(motionSensePin), digitalRead(motionSensePin_2));
@@ -141,7 +171,7 @@ void handleMotionStatus()
     {
       lastCheckStatus = digitalRead(motionSensePin);
 
-      // set led 
+      // set led
       digitalWrite(D0, lastCheckStatus);
 
       // send message to websocket
